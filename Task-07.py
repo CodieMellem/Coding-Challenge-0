ab334e0483fac19c6116b36670051a1fbfbf2e09
Codie Mellem <codiemellem1@gmail.com>
2021-03-12 19:54:34 +0200

    fixing output

--- Task-07.py
+++ Task-07.py
@@ -1,10 +1,10 @@
 def convert_to_fahrenheit(celsius):
     fahren = celsius * 9/5 +32
-    return (celsius + 'C = ' + fahren + 'F')
+    return ("{}C = {}F".format(celsius, fahren))
 
 def convert_to_celsius(fahrenheit):
     cels = (fahrenheit - 32)/1.8
-    return (fahrenheit + 'F = ' + cels + 'C')
+    return ("{}F = {}C".format(fahrenheit, cels))
     
 print(convert_to_fahrenheit(20))
 print(convert_to_celsius(68))
