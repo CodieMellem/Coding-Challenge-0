2894e6a3daf891511a59921abd149533b3fb8b1c
Codie Mellem <codiemellem1@gmail.com>
2021-03-12 19:49:30 +0200

    making code more understandable

--- Task-07.py
+++ Task-07.py
@@ -1,10 +1,10 @@
 def convert_to_fahrenheit(celsius):
     fahren = celsius * 9/5 +32
-    return fahren
+    return (celsius + 'C = ' + fahren + 'F')
 
 def convert_to_celsius(fahrenheit):
     cels = (fahrenheit - 32)/1.8
-    return cels
+    return (fahrenheit + 'F = ' + cels + 'C')
     
 print(convert_to_fahrenheit(20))
 print(convert_to_celsius(68))
