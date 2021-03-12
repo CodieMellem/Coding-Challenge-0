4e80965351a30255fdb119ee12deb712a03d908a
Codie Mellem <codiemellem1@gmail.com>
2021-03-12 21:42:01 +0200

    Making code more understandable

--- Task-07.py
+++ Task-07.py
@@ -1,10 +1,15 @@
 def convert_to_fahrenheit(celsius):
-    fahren = celsius * 9/5 + 32
+    fahren = celsius * 9/5 + 32      #Formula for converting fahrenheit to celsius
     return ("{}C = {}F".format(celsius, fahren))
 
 def convert_to_celsius(fahrenheit):
-    cels = (fahrenheit - 32)/1.8
+    cels = (fahrenheit - 32)/1.8     #Formula for converting Celsius to fahrenheit
     return ("{}F = {}C".format(fahrenheit, cels))
     
 print(convert_to_fahrenheit(20))
 print(convert_to_celsius(68))
+
+"""
+need more feedback on what type of changes
+are needed to make this code more understandable
+"""
