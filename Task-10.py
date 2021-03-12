50179688024cd150459477d386852820b7663d22
Codie Mellem <codiemellem1@gmail.com>
2021-03-12 19:26:52 +0200

    changes to task-10

--- Task-10.py
+++ Task-10.py
@@ -1,23 +1,18 @@
 def common_char(word_one, word_two):
-
     word_one = word_one.lower()
     word_two = word_two.lower()
 
     common = ''
     for character_one in word_one:
-
         for character_two in word_two:
-
             if character_one == character_two:
-                common += character_one
-
-       
+                common += character_one 
+    """      
     common_letters = ''
     for letters in common:
         if not letters in common_letters:
             common_letters += letters + ' '
-
-    print("Common Letters : " + common_letters)
-    
-    
+    """
+    print('Common Letters : ' + ', '.join(common))
+       
 common_char("house", "computers")
