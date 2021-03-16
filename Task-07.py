bb0c8a9e9e07de0feb90e938c9dd2bf8957be418
Codie Mellem <codiemellem1@gmail.com>
2021-03-16 08:01:06 +0200

    changes to shortened variable names to make more readable

--- Task-07.py
+++ Task-07.py
@@ -1,15 +1,11 @@
 def convert_to_fahrenheit(celsius):
-    fahren = celsius * 9/5 + 32      #Formula for converting fahrenheit to celsius
-    return ("{}C = {}F".format(celsius, fahren))
+    fahrenheit = celsius * 9/5 + 32      
+    return fahrenheit
 
 def convert_to_celsius(fahrenheit):
-    cels = (fahrenheit - 32)/1.8     #Formula for converting Celsius to fahrenheit
-    return ("{}F = {}C".format(fahrenheit, cels))
+    celsius = (fahrenheit - 32)/1.8    
+    return celsius
     
 print(convert_to_fahrenheit(20))
 print(convert_to_celsius(68))
 
-"""
-need more feedback on what type of changes
-are needed to make this code more understandable
-"""
